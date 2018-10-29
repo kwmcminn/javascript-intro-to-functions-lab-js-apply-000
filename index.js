7624308a5c36f4e98be5b27c4c4880a92adea065
Kevin McMinn <kwmcminn@gmail.com>
2018-10-29 13:19:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if sayHiToGrandma.toLowerCase()
+  if string === string.toLowerCase()
+  return "I can't hear you!"
 }
